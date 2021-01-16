8bbe79cdca2cfe2946d40fa6c02ab29264da0a2d
gokhansarapevi <gokhansarapevi@gmail.com>
2021-01-16 23:47:06 +0300

    16 Ocak Ders Sonu

---
+++ Methods/CartManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    class CartManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine("Added to Cart : " + product.Name);
+        }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Name = "Elma";
+            product1.Price = 15;
+            product1.Description = "Amasya Elması";
+            product1.UnitsInStock = 39;
+
+            Product product2 = new Product();
+            product2.Name = "Çilek";
+            product2.Price = 10;
+            product2.Description = "Dağ çileği";
+            product2.UnitsInStock = 100;
+
+            Product[] products = new Product[] { product1, product2, };
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Price);
+                Console.WriteLine(product.Description);
+                Console.WriteLine(product.UnitsInStock);
+                Console.WriteLine("--------");
+            };
+
+            Console.WriteLine("------Metotlar-------");
+            CartManager cartManager = new CartManager();
+            cartManager.Add(product1);
+            cartManager.Add(product2);
+
+        }
+    }
+}
